ef5df89786de6e9a78e081710f3d4e78bd0dac81
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-06-22 22:37:32 -0500

    adds gray matter

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,21 +1,26 @@
 import React from 'react';
 import fs from 'fs';
 import path from 'path';
+import matter from 'gray-matter';
+import Head from 'next/head';
 
-const Post = ({ slug, contents }) => {
+const Post = ({ slug, content, data }) => {
   return (
-    <div>
+    <>
+      <Head>
+        <title>{data.title}</title>
+      </Head>
       <div>
         Hello, the slug for this is: {slug}
       </div>
       <pre>
-       {contents}
+       {content}
       </pre>
-    </div>
+    </>
   );
 };
 
-export const getStaticPaths = async () => {
+export const getStaticPaths = () => {
   const files = fs.readdirSync('posts');
   const paths = files.map(fileName => ({
     params: {
@@ -29,13 +34,16 @@ export const getStaticPaths = async () => {
   };
 };
 
-export const getStaticProps = async ({ params: { slug } }) => {
-  const contents = fs.readFileSync(path.join('posts', `${slug}.md`)).toString();
+export const getStaticProps = ({ params: { slug } }) => {
+  const rawMarkdown = fs.readFileSync(path.join('posts', `${slug}.md`)).toString();
+  const parsedMarkdown = matter(rawMarkdown);
+  const { content, data } = parsedMarkdown;
 
   return {
     props: {
       slug,
-      contents,
+      content,
+      data,
     },
   };
 };
