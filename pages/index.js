66d3321e70b0be04c5c1a73c1c7178ddcab7075d
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-06-22 22:50:20 -0500

    adds dynamic generated links

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,30 @@
 import Link from 'next/link';
+import fs from 'fs';
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="container">
-      <Link href="/faq">
-        <a>go to faq</a>
-      </Link>
+      <ul>
+      </ul>
+      {
+        posts.map(post => (
+        <li key={post}>
+          <Link href={`blog/${post}`}>
+            <a>go to {post}</a>
+          </Link>
+        </li>
+        ))
+      }
     </div>
-  )
-}
+  );
+};
+
+export const getStaticProps = () => {
+  const files = fs.readdirSync('posts');
+
+  return {
+    props: {
+      posts: files.map(filename => filename.replace('.md', '')),
+    },
+  };
+};
